fd8d21d7e2cdc35a47a390c107313d1544d0e95d
Eugene Derevyanko <eugene.derevyanko@gmail.com>
2014-07-30 13:59:34 +0300

    using text value for HTmlHeader and footer

--- src/main/java/com/github/symulakr/gwt/generators/rebind/celltable/extractor/HeaderExtractor.java
+++ src/main/java/com/github/symulakr/gwt/generators/rebind/celltable/extractor/HeaderExtractor.java
@@ -23,11 +23,7 @@ public class HeaderExtractor
       if (annotatedMethod.isAnnotationPresent(HtmlHeader.class))
       {
          HtmlHeader annotation = annotatedMethod.getAnnotation(HtmlHeader.class);
-         if (useUiObject(context, annotation.uiObject()))
-         {
-            return context;
-         }
-         useStringValue(context, annotation.value(), annotation.text());
+         fillContext(context, annotation.uiObject(), annotation.value(), annotation.text());
       }
       else
       {
@@ -42,17 +38,26 @@ public class HeaderExtractor
       if (annotatedMethod.isAnnotationPresent(HtmlFooter.class))
       {
          HtmlFooter annotation = annotatedMethod.getAnnotation(HtmlFooter.class);
-         if (useUiObject(context, annotation.uiObject()))
-         {
-            return context;
-         }
-         useStringValue(context, annotation.value(), annotation.text());
+         fillContext(context, annotation.uiObject(), annotation.value(), annotation.text());
       }
       else
       {
          useSimpleFooter(context, annotatedMethod);
       }
       return context;
+   }
+
+   private void fillContext(HeaderContext context, Class<? extends UIObject> uiObject, Class<? extends Header> header, String text)
+   {
+      if (useUiObject(context, uiObject, text))
+      {
+         return;
+      }
+      if (useHeaderObject(context, header, text))
+      {
+         return;
+      }
+      context.setStringValue(text);
    }
 
    private void useSimpleHeader(HeaderContext context, JMethod annotatedMethod)
@@ -67,11 +72,22 @@ public class HeaderExtractor
       context.setStringValue(StringUtils.isNotEmpty(column.footer()) ? column.footer() : null);
    }
 
-   private boolean useUiObject(HeaderContext context, Class<? extends UIObject> uiObject)
+   private boolean useUiObject(HeaderContext context, Class<? extends UIObject> uiObject, String text)
    {
       if (!DefaultValue.DEFAULT_UIOBJECT_TYPE.equals(uiObject))
       {
          context.setHeaderUiObject(uiObject);
+         useStringValue(context, uiObject, text);
+         return true;
+      }
+      return false;
+   }
+
+   private boolean useHeaderObject(HeaderContext context, Class<? extends Header> header, String text)
+   {
+      if (useStringValue(context, header, text) || ReflectUtils.hasDefaultConstructor(header))
+      {
+         context.setHeaderValue(header);
          return true;
       }
       return false;
@@ -82,26 +98,19 @@ public class HeaderExtractor
       return StringUtils.isNotEmpty(candidate) ? candidate : annotatedMethod.getName();
    }
 
-   private void useStringValue(HeaderContext context, Class<? extends Header> value, String text)
+   private boolean useStringValue(HeaderContext context, Class clazz, String text)
    {
-      if (hasConstructorBySafeHtml(value) && StringUtils.isNotEmpty(text))
-      {
-         context.setHeaderValue(value);
-         context.setStringValue(text);
-      }
-      else if (ReflectUtils.hasDefaultConstructor(value))
-      {
-         context.setHeaderValue(value);
-      }
-      else
+      if (hasConstructorBySafeHtml(clazz) && StringUtils.isNotEmpty(text))
       {
          context.setStringValue(text);
+         return true;
       }
+      return false;
    }
 
-   private boolean hasConstructorBySafeHtml(Class<? extends Header> value)
+   private boolean hasConstructorBySafeHtml(Class clazz)
    {
-      Constructor[] constructors = value.getDeclaredConstructors();
+      Constructor[] constructors = clazz.getDeclaredConstructors();
       for (Constructor constructor : constructors)
       {
          Class[] parameters = constructor.getParameterTypes();
